ed24b83f4dc83a2c6d1b84e632ba6d384c87cc13
rajat sardesai <rajatsardesai@gmail.com>
2023-05-12 17:01:00 +0530

    fix: ui fix complete

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,25 +5,24 @@ export default function MovieList(props) {
     const contexts = useContext(movieContext);
     const { addFavouriteMovie, removeFavouriteMovie, AddFavourites, RemoveFavourites } = contexts;
     const { isFavourites } = props;
+
     return (
         <>
             {props.movies.map((movie, index) => {
                 return (
                     movie.Poster ?
-                        <div className="col-md-3 col-12" key={index}>
-                            <div className="image-container d-flex justify-content-start">
-                                <img src={movie.Poster !== "N/A" ? movie.Poster : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"} alt={movie.Title} />
-                                <div className="overlay d-flex justify-content-center align-items-center" onClick={() => !isFavourites ? addFavouriteMovie(movie) : removeFavouriteMovie(movie)}>
-                                    {
-                                        !isFavourites ?
-                                            <>
-                                                <AddFavourites />
-                                            </>
-                                            : <>
-                                                <RemoveFavourites />
-                                            </>
-                                    }
-                                </div>
+                        <div className="image-container d-flex justify-content-start m-4" key={index}>
+                            <img src={movie.Poster !== "N/A" ? movie.Poster : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"} alt={movie.Title} className="rounded" />
+                            <div className="overlay d-flex justify-content-center align-items-center" onClick={() => !isFavourites ? addFavouriteMovie(movie) : removeFavouriteMovie(movie)}>
+                                {
+                                    !isFavourites ?
+                                        <>
+                                            <AddFavourites />
+                                        </>
+                                        : <>
+                                            <RemoveFavourites />
+                                        </>
+                                }
                             </div>
                         </div>
                         : <p className="text-light" key={index}>{movie}</p>
